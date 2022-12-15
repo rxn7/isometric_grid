3a72d938a7164a5721b34e932d9abe44b58603ce
rxtthin <rotthin_dev@protonmail.com>
2022-12-15 13:15:54 +0100

    Minify static files on build

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ mkdir build
 npx tsc --project $(pwd)
 cp -r static/* build
 
-# Optimize css
-tr --delete '\n' < static/style.css > build/style.css
+minify static/index.html -o build/index.html
+minify static/style.css -o build/style.css
 
 echo -e "\e[1;32mProject has been successfuly built!\e[0m"
