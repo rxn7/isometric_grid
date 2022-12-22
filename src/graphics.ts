8e072eff9ce3903415d921848b7214cc900e96c1
rxtthin <rotthin_dev@protonmail.com>
2022-12-22 14:56:06 +0100

    Don't alpha for 2D context

--- src/graphics.ts
+++ src/graphics.ts
@@ -1,12 +1,13 @@
 export namespace Graphics {
 	export const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement
-	export const ctx: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D
+	export const ctx: CanvasRenderingContext2D = canvas.getContext('2d', { alpha: false }) as CanvasRenderingContext2D
 
 	export function init(): void {
 		if (!ctx) throw new Error('Failed to get canvas context')
 	}
 
 	export function clear(): void {
-		ctx.clearRect(0.0, 0.0, canvas.clientWidth, canvas.clientHeight)
+		ctx.fillStyle = 'skyblue'
+		ctx.fillRect(0.0, 0.0, canvas.clientWidth, canvas.clientHeight)
 	}
 }
