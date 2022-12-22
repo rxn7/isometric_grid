bad8f9ab80b4eaf9269b1f30ab80ffc998aae3fa
rxtthin <rotthin_dev@protonmail.com>
2022-12-22 14:56:11 +0100

    Updates

--- src/graphics.js
+++ src/graphics.js
@@ -1,14 +1,15 @@
 export var Graphics;
 (function (Graphics) {
     Graphics.canvas = document.getElementById('canvas');
-    Graphics.ctx = Graphics.canvas.getContext('2d');
+    Graphics.ctx = Graphics.canvas.getContext('2d', { alpha: false });
     function init() {
         if (!Graphics.ctx)
             throw new Error('Failed to get canvas context');
     }
     Graphics.init = init;
     function clear() {
-        Graphics.ctx.clearRect(0.0, 0.0, Graphics.canvas.clientWidth, Graphics.canvas.clientHeight);
+        Graphics.ctx.fillStyle = 'skyblue';
+        Graphics.ctx.fillRect(0.0, 0.0, Graphics.canvas.clientWidth, Graphics.canvas.clientHeight);
     }
     Graphics.clear = clear;
 })(Graphics || (Graphics = {}));
